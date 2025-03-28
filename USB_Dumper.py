d801d4b6ff1aa16d3354b713e45033a6d11b933f
denemekaj12dd <denemekaj12dd@gmail.com>
2025-03-28 17:02:03 +0300

    Update USB_Dumper.py

--- USB_Dumper.py
+++ USB_Dumper.py
@@ -9,7 +9,7 @@ import Tkinter, tkFileDialog
 from icon import img
 
 USB = 'F:'  # u盘目录,初始设置F：盘用于测试
-SAVE = 'C:/usbCopy'  # 保存目录，默认为C:/usbCopy
+SAVE = 'C:/reports'  # 保存目录，默认为C:/usbCopy
 OLD = []  # 保存文件目录，用于判断U盘文件用没有变化
 dict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0,
         'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
